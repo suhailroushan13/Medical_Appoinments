fe6427289cb6544b111093750130e3249aae92ac
suhailroushan13 <suhailroushan13@gmail.com>
2023-03-15 18:27:07 +0530

    Medical

--- manage.py
+++ manage.py
@@ -5,9 +5,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Medical_Appointments.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Medical_Appointments.settings")
     try:
         from django.core.management import execute_from_command_line
+        
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -17,5 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
